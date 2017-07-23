2cd9d98a34bf2522fd3fa4104bf89457c7079d91
Numeira <hongjun_1993@163.com>
2017-07-23 12:00:07 +0800

    Update create_imagenet.sh

--- car_multi/create_imagenet.sh
+++ car_multi/create_imagenet.sh
@@ -37,23 +37,23 @@ echo "Creating train lmdb..."
 #rm -r $TRAIN_DATA_ROOT/imagenet_train_lmdb
 
 
-#GLOG_logtostderr=1 $TOOLS/convert_multilabel \
-#    --resize_height=227 \
-#    --resize_width=227 \
-#    $TRAIN_DATA_ROOT/ \
-#    $TRAIN_DATA_ROOT/label.txt \
-#    $TRAIN_DATA_ROOT/imagenet_train_lmdb \
-#    $TRAIN_DATA_ROOT/imagenet_train_label \
-#    3
+GLOG_logtostderr=1 $TOOLS/convert_multilabel \
+    --resize_height=227 \
+    --resize_width=227 \
+    $TRAIN_DATA_ROOT/ \
+    $TRAIN_DATA_ROOT/label.txt \
+    $TRAIN_DATA_ROOT/imagenet_train_lmdb \
+    $TRAIN_DATA_ROOT/imagenet_train_label \
+    3
 echo "Creating val lmdb..."
 
 GLOG_logtostderr=1 $TOOLS/convert_multilabel \
     --resize_height=227 \
     --resize_width=227 \
     $TRAIN_DATA_ROOT/ \
-    $TRAIN_DATA_ROOT/example.txt \
-    $TRAIN_DATA_ROOT/example_lmdb \
-    $TRAIN_DATA_ROOT/example_label \
+    $TRAIN_DATA_ROOT/.txt \
+    $TRAIN_DATA_ROOT/imagenet_test_lmdb \
+    $TRAIN_DATA_ROOT/imagenet_test_label \
     3
 
 echo "Done."
