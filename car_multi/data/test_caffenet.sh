700f0c5266f647357b5f821becf347859e27b407
Numeira <hongjun_1993@163.com>
2017-07-23 12:54:08 +0800

    Update test_caffenet.sh

--- car_multi/data/test_caffenet.sh
+++ car_multi/data/test_caffenet.sh
@@ -4,6 +4,6 @@ set -e
 /root/caffe/build/tools/classification_multilabel \
 /data1/liangdas/multi_labels/caffe_script/car_multi/data/deploy.prototxt \
 /data1/liangdas/multi_labels/caffe_script/car_multi/data/googLenet_fine_tuning/googLenet_iter_300000.caffemodel \
-/data1/liangdas/multi_labels/caffe_script/car_multi/data/example_mean.binaryproto \
+/data1/liangdas/multi_labels/caffe_script/car_multi/data/imagenet_mean.binaryproto \
 label3.txt label2.txt label1.txt \
 /data1/liangdas/multi_labels/caffe_script/car_multi/data/003913.jpg  $@
